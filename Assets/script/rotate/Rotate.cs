91685f4ce3b7bcb3cbe7405b3ab94996ef1f6bf7
DongYang <404765437@qq.com>
2016-03-18 13:56:05 +0800

    夏威夷学习场景

---
+++ Assets/script/rotate/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public Transform cube;
+    public Transform sphere;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cube.Rotate(Vector3.up, 2.5f,Space.World);
+	}
+}
